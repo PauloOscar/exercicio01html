c59ec393ded36206deb7223a7363d2999487b9e8
Oscar /oliveira <oscarslnh@gmail.com>
2021-08-02 22:51:32 -0300

    estruturação de pasta

---
+++ EXERCICIOS/03_JS/aula-14/aula-14.js
@@ -0,0 +1,23 @@
+console.log("exempla A")
+for(var i = 0 < 10; i++){
+    console.log(i);
+}
+console.log("exemplo B")
+for(var i = 1; i < = 10; i++){
+    console.log(i);
+}
+console.log("exemplo /c")
+
+console.log("exemplo D")
+var soma = 0;
+for(var i =0; i < 100; i++){
+
+    if(i%4==0){
+        soma += i;
+        console.log(soma)
+
+    }
+    
+}
+console.log(soma);
+
