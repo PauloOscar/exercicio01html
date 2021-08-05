6df43dda5ddcee28dfc89e15e3b681747ca0793f
Oscar /oliveira <oscarslnh@gmail.com>
2021-08-05 18:06:01 -0300

    js atualizado

--- EXERCICIOS/03_JS/aula-14/aula-14.js
+++ EXERCICIOS/03_JS/aula-14/aula-14.js
@@ -1,24 +1,48 @@
-console.log("exempla A")
-for(var i = 0 < 10; i++){
-    console.log(i);
-}
-console.log("exemplo B")
-for(var i = 1; i < = 10; i++){
-    console.log(i);
-}
-console.log("exemplo /c")
+// console.log("exemplo A")
+// for( var i = 1,; i <= 10; i++){
+// console.log(i);
+// }
+// console.log("")
 
-console.log("exemplo D")
-var soma = 0;
-for(var i =0; i < 100; i++){
+// console.log("exemplo B")
+// for( var i = 1; i<= 10; i++){
+//     console.log(i);
+// }
+// console.log("")
 
-    if(i%4==0){
-        soma += i;
-        console.log(soma)
+// console.log("exemplo C")
+// for(var i = 10; i>= 0; i--){
+//     console.log(i);
+// }
 
-    }
+// console.log("")
+
+// console.log("exemplo D")
+// var soma=0;
+// for(var i = 0; i < 100; i++){
+
+//     if(i%3==0){
+//         console.log(i);
+//     }
+// }
+// console.log("exemplo E")
+// var soma= 0;
+// for(var i = 0; i < 100; i++){
     
+//     if(i%4==0){
+//         soma+= i;
+//         console.log(soma)
+//     }
+// }
+// console.log(soma);
 
-}
+// console.log("exemplo F")
+// var soma=0
+// for(var linha = 1; linha <=3; linha++){
+
+//     for(var coluna = 1; coluna <=4; coluna++){
+//         console.log(`L. ${linha} C. ${coluna}`)
+        
+//     }
+// }
 // console.log(soma); 
-
